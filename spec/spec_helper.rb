9fa18425eaa1296fed57ce38dbb2ecba135c8ec0
johnotander <johnotander@gmail.com>
2013-06-20 14:50:56 -0600

    Raise rspec error without ecm credentials.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,3 +15,7 @@ USERNAME ||= ENV['ecm_username']
 PASSWORD ||= ENV['evm_password']
 
 raise 'REQUIRED ENV variables: [ecm_username and ecm_password] in order to run the specs.' unless USERNAME and PASSWORD
+
+def authenticate_with_valid_credentials
+  Cradlepointr.authenticate(USERNAME, PASSWORD)
+end
