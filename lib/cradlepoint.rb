a5850f67ae622a26378730eac3220d237c82a346
johnotander <johnotander@gmail.com>
2013-08-20 10:40:26 -0600

    Grab error code correctly.

--- lib/cradlepoint.rb
+++ lib/cradlepoint.rb
@@ -33,8 +33,8 @@ module Cradlepoint
     
     handle_response(response)
   rescue RestClient::Exception => e
-    puts "RestClient::Exception received: #{ e.code }"
-    return case e.code
+    puts "RestClient::Exception received: #{ e.response.code }"
+    return case e.response.code
            when 400 then { success: false, error_code: 400, error: e }
            when 401 then { success: false, error_code: 401, error: e }
            when 403 then { success: false, error_code: 403, error: e }
