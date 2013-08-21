fd238b03a29f286e3c39c60daad5075858272c88
johnotander <johnotander@gmail.com>
2013-08-21 16:09:39 -0600

    Break hash_helpers into utils module.

--- lib/utils/hash_helpers.rb
+++ lib/utils/hash_helpers.rb
@@ -1,25 +1,23 @@
-module Cradlepoint
-  module Utils
-    module HashHelpers
-      # From 
-      # http://devblog.avdi.org/2009/07/14/recursively-symbolize-keys/
-      def self.symbolize_keys(hash)
-        if hash.is_a?(Array)
-          hash.map { |h| symbolize_keys(h) }
-        else
-          hash.inject({}) { |result, (key, value)|
-            new_key = case key
-                      when String then key.to_sym
-                      else key
+module Utils
+  module HashHelpers
+    # From 
+    # http://devblog.avdi.org/2009/07/14/recursively-symbolize-keys/
+    def self.symbolize_keys(hash)
+      if hash.is_a?(Array)
+        hash.map { |h| symbolize_keys(h) }
+      else
+        hash.inject({}) { |result, (key, value)|
+          new_key = case key
+                    when String then key.to_sym
+                    else key
+                    end
+          new_value = case value
+                      when Hash then symbolize_keys(value)
+                      else value
                       end
-            new_value = case value
-                        when Hash then symbolize_keys(value)
-                        else value
-                        end
-            result[new_key] = new_value
-            result
-          }
-        end
+          result[new_key] = new_value
+          result
+        }
       end
     end
   end
